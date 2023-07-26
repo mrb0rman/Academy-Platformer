be0f8103327cd109011226971c5493b8995f863f
unknown <l>
2023-07-26 14:24:30 +0700

    Add Avstract class for all windows. Add DOTween plugin. Add DOTween open/close animations.

--- Assets/Scripts/UIWindow.cs
+++ Assets/Scripts/UIWindow.cs
@@ -1,9 +1,21 @@
+using System;
 using UnityEngine;
+using DG.Tweening;
 
 namespace DefaultNamespace
 {
     public abstract class UIWindow : MonoBehaviour
     {
+        private float duration = 1f;
         
+        public void CloseAnimation()
+        {
+            transform.DOScale(Vector3.zero, duration);
+        }
+
+        public void OpenAnimation()
+        {
+            transform.DOScale(Vector3.one, duration);
+        }
     }
 }
