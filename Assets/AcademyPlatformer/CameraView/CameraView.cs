00c3341921f0be14dd9c5063c41095e75594c570
AnnaNovikova <novikanna588@gmail.com>
2023-07-28 12:45:57 +0700

    Delete space
    Не понимаю комментарий "публичные свойства идут на приватными полями". Приватное свойство стоит для prefab камеры, для нее создается метод get для работы с этим prefab извне

--- Assets/AcademyPlatformer/CameraView/CameraView.cs
+++ Assets/AcademyPlatformer/CameraView/CameraView.cs
@@ -1,10 +1,8 @@
-using System;
 using UnityEngine;
 
 public class CameraView : MonoBehaviour
 {
     [SerializeField] private Camera _mainCamera;
-
+    
     public Camera MainCamera => _mainCamera;
-
 }
