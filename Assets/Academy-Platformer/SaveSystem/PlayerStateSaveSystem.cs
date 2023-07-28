d25a87596849e198ed015233176b40fefa4d33f6
Boomrock <63188345+Boomrock@users.noreply.github.com>
2023-07-28 11:22:10 +0700

    Delete abstract class PlayerPrefSaveSystem

--- Assets/Academy-Platformer/SaveSystem/PlayerStateSaveSystem.cs
+++ Assets/Academy-Platformer/SaveSystem/PlayerStateSaveSystem.cs
@@ -2,24 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStateSaveSystem : PlayerPrefSaveSystem<PlayerState>
+public class PlayerStateSaveSystem : ISaveSystem<PlayerState>
 {
     private const string Key = "PlayerStateKey";
     void Start()
     {
         Load();
     }
-    public override void Save(PlayerState date)
+    public  void Save(PlayerState date)
     {
         Save(key: Key, date: date);
     }
-    public override PlayerState Load()
+    public PlayerState Load()
     {
         return Load(key: Key);
     }
-    public override void Clear()
+    public  void Clear()
     {
         Clear(key: Key);
+    }
+    protected void Save(string key, PlayerState date)
+    {
+        string json = JsonUtility.ToJson(date);
+        PlayerPrefs.SetString(key: key, value: json);
+    }
+    protected PlayerState Load(string key)
+    {
+        if (PlayerPrefs.HasKey(key))
+        {
+            return default;
+        }
+        string json = PlayerPrefs.GetString(key);
+        var date = JsonUtility.FromJson<PlayerState>(json);
+        return date;
+    }
+    protected void Clear(string key)
+    {
+        if (PlayerPrefs.HasKey(key))
+        {
+            PlayerPrefs.DeleteKey(key);
+        }
     }
 }
 
