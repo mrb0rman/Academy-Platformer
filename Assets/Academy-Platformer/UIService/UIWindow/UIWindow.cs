02635772f30dfb2542ae18b5582486b7a6433180
unknown <l>
2023-07-31 11:56:55 +0700

    Rename constant in UIWindow

--- Assets/Academy-Platformer/UIService/UIWindow/UIWindow.cs
+++ Assets/Academy-Platformer/UIService/UIWindow/UIWindow.cs
@@ -6,20 +6,20 @@ namespace UIService
 {
     public abstract class UIWindow : MonoBehaviour, IUIWindow
     {
-        private const float duration = 1f;
+        private const float Duration = 1f;
         private Tween _showAnimation;
         private Tween _hideAnimation;
         
         public void Show()
         {
             _showAnimation?.Kill();
-            _showAnimation = this.transform.DOScale(Vector3.one, duration);
+            _showAnimation = this.transform.DOScale(Vector3.one, Duration);
         }
 
         public void Hide()
         {
             _hideAnimation?.Kill();
-            _hideAnimation = this.transform.DOScale(Vector3.zero, duration);
+            _hideAnimation = this.transform.DOScale(Vector3.zero, Duration);
         }
     }
 }
