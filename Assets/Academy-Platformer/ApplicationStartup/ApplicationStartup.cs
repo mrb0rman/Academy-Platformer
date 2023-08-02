606dee6632eea2b1daf51ace7212ee4587cbf8fa
Keinih Danil <keinihd@gmail.com>
2023-08-02 11:04:43 +0700

    add ApplicationStartup

---
+++ Assets/Academy-Platformer/ApplicationStartup/ApplicationStartup.cs
@@ -0,0 +1,21 @@
+﻿using Bootstrap;
+using UnityEngine;
+
+namespace ApplicationStartup
+{
+    public class ApplicationStartup : MonoBehaviour
+    {
+        private IBootstrap _bootstrap = new Bootstrap.Bootstrap();
+        
+        private void Start()
+        {
+            Destroy(gameObject);
+        }
+        
+        private void NotifyOfCompletion()
+        {
+            Debug.Log("The end");
+        }
+        
+    }
+}
