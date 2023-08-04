533c732e9b8f4f5a18caa3019d11a0753adc9b8b
cherkasov <cherkasov_danil@smartworld.team>
2023-08-04 14:48:17 +0700

    remove space and add new gitignore

--- Assets/Academy-Platformer/PlayerMVC/PlayerConfig.cs
+++ Assets/Academy-Platformer/PlayerMVC/PlayerConfig.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 namespace FactoryPlayer
 {
     [CreateAssetMenu(fileName = "PlayerConfig", menuName = "Configs/PlayerConfig", order = 0)]
-    
     public class PlayerConfig : ScriptableObject
     {
         public PlayerModel PlayerModel => playerModel;
