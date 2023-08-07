4a001e51c96b4ca5eb8dfe8f32cbf56895172c68
lyundup_buyan <blyundup@gmail.com>
2023-08-07 10:27:19 +0700

    fix player movement

--- Assets/Academy-Platformer/Player/PlayerMovementController.cs
+++ Assets/Academy-Platformer/Player/PlayerMovementController.cs
@@ -6,12 +6,16 @@ namespace Academy.Platformer.Player
 {
     public class PlayerMovementController : MonoBehaviour
     {
+        [SerializeField] private float speed = 1f;
+        
         private readonly InputController _inputController;
         private readonly TemporaryPlayerView _temporaryPlayerView;
+        
+        private const float Epsilon = 0.1f;
+        
         private float _playerLength;
         private float _cameraHalfWidth;
         private float _offsetFromEdge;
-        private const float Epsilon = 0.1f;
 
         public PlayerMovementController(
             InputController inputController,
@@ -27,22 +31,24 @@ namespace Academy.Platformer.Player
             _inputController.OnLeftEvent.AddListener(MoveLeft);
             _inputController.OnRightEvent.AddListener(MoveRight);
         }
-
+        
         private void MoveLeft()
         {
-            var playerLeftEdgeX = transform.position.x - _playerLength / 2;
-            if (playerLeftEdgeX + _cameraHalfWidth > Epsilon)
-                transform.DOMove(Vector3.left, 1f);
+            var playerLeftEdgeX = transform.position.x - _offsetFromEdge;
+            if (playerLeftEdgeX + Vector3.left.x * speed + _cameraHalfWidth > Epsilon)
+                transform.DOMove(transform.position + Vector3.left * speed, 1f);
             else
-                transform.position = new Vector3(-_cameraHalfWidth + _offsetFromEdge, transform.position.y, 0f);
+                transform.DOMove(new Vector3(-_cameraHalfWidth + _offsetFromEdge, 
+                    transform.position.y, 0f), 1f);
         }
         private void MoveRight()
         {
-            var playerRightEdgeX = transform.position.x + _playerLength / 2;
-            if (_cameraHalfWidth - playerRightEdgeX > Epsilon)
-                transform.DOMove(Vector3.right, 1f);
+            var playerRightEdgeX = transform.position.x + _offsetFromEdge;
+            if (_cameraHalfWidth + Vector3.left.x * speed - playerRightEdgeX > Epsilon)
+                transform.DOMove(transform.position + Vector3.right * speed, 1f);
             else
-                transform.position = new Vector3(_cameraHalfWidth - _offsetFromEdge, transform.position.y, 0f);
+                transform.DOMove(new Vector3(_cameraHalfWidth - _offsetFromEdge, 
+                    transform.position.y, 0f), 1f);
         }
     }
 }
