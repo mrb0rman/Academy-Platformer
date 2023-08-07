175c60f9f3ff11be0ada42598fcaa38d782a7658
Keinih Danil <keinihd@gmail.com>
2023-08-07 12:01:05 +0700

    add Player Storage

---
+++ Assets/Academy-Platformer/PlayerStorage/PlayerStorage.cs
@@ -0,0 +1,46 @@
+﻿using FactoryPlayer;
+using UnityEngine;
+
+namespace PlayerStorage
+{
+    public class PlayerStorage
+    {
+        public PlayerView PlayerView
+        {
+            get
+            {
+                if (_playerView == null)
+                {
+                    Debug.LogError("Player is null");
+                }
+                
+                return _playerView;
+            }
+            set
+            {
+                if(value == null)
+                {
+                    Debug.LogError("Player is null");
+                
+                    return;
+                }
+
+                _playerView = value;
+            }
+        }
+
+        private PlayerView _playerView;
+
+        public void Delete()
+        {
+            if(PlayerView == null)
+            {
+                Debug.LogError("Player is null");
+                
+                return;
+            }
+
+            Object.Destroy(PlayerView);
+        }
+    }
+}
