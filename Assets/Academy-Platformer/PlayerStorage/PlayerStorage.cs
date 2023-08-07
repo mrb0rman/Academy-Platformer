42910afe7edc38ee7c0ef205a3c7368bcaa9516c
Keinih Danil <keinihd@gmail.com>
2023-08-07 13:41:42 +0700

    fix PlayerStorage

--- Assets/Academy-Platformer/PlayerStorage/PlayerStorage.cs
+++ Assets/Academy-Platformer/PlayerStorage/PlayerStorage.cs
@@ -1,46 +1,54 @@
-﻿using FactoryPlayer;
+﻿using System.Collections.Generic;
+using FactoryPlayer;
 using UnityEngine;
 
 namespace PlayerStorage
 {
     public class PlayerStorage
     {
-        public PlayerView PlayerView
+        private Dictionary<int, Component> _dictObject = new Dictionary<int, Component>();
+
+        public void Add(Component playerView)
         {
-            get
+            if (playerView == null)
             {
-                if (_playerView == null)
-                {
-                    Debug.LogError("Player is null");
-                }
-                
-                return _playerView;
-            }
-            set
-            {
-                if(value == null)
-                {
-                    Debug.LogError("Player is null");
-                
-                    return;
-                }
-
-                _playerView = value;
-            }
-        }
-
-        private PlayerView _playerView;
-
-        public void Delete()
-        {
-            if(PlayerView == null)
-            {
-                Debug.LogError("Player is null");
-                
+                Debug.LogError($"Component {playerView} is null");
                 return;
             }
 
-            Object.Destroy(PlayerView);
+            if (_dictObject.ContainsKey(playerView.GetInstanceID()))
+            {
+                Debug.LogError($"Component {playerView} has already been written to the dictionary");
+                return;
+            }
+            
+            _dictObject.Add(playerView.GetInstanceID(), playerView);
+        }
+
+        public T Get<T>(int id) where T: Component
+        {
+            if (!_dictObject.ContainsKey(id))
+            {
+                Debug.LogError($"the component with this id = {id} is not in the dictionary"); 
+                
+                return null;
+            }
+
+            return (T)_dictObject[id];
+        }
+        
+        public void Delete(int id)
+        {
+            if (!_dictObject.ContainsKey(id))
+            {
+                Debug.LogError($"the component with this id = {id} is not in the dictionary"); 
+                
+                return;
+            }
+            
+            Object.Destroy(_dictObject[id]);
+            
+            _dictObject.Remove(id);
         }
     }
 }
