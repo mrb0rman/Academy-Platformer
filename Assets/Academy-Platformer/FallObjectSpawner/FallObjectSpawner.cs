7d41091f45d8a295c701410f3a3b9fac0394d8ca
unknown <l>
2023-08-10 14:33:07 +0700

    Create spawner of falling objects
    Add class FallObjectSpawner. Create Prefab for spawner. Create Scene for testing.

---
+++ Assets/Academy-Platformer/FallObjectSpawner/FallObjectSpawner.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Academy_Platformer.FallObject;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FallObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] spawnPoints;
+
+    [SerializeField] private float spawnPeriodMin;
+    
+    [SerializeField] private float spawnPeriodMax;
+
+    private int _typesCount;
+
+    private float _spawnPeriod;
+
+    private FallObjectPool _pool;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pool = new FallObjectPool(new FallObjectController());
+        _spawnPeriod = Random.Range(spawnPeriodMin, spawnPeriodMax);
+        _typesCount = Enum.GetValues(typeof(FallObjectType)).Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _spawnPeriod -= Time.deltaTime;
+
+        if (_spawnPeriod <= 0)
+        {
+            SpawnNewObject(Random.Range(0, 3));
+            _spawnPeriod = Random.Range(spawnPeriodMin, spawnPeriodMax);
+        }
+    }
+
+    private void SpawnNewObject(int spawnPoint)
+    {
+        var type = Random.Range(0, _typesCount);
+        var newObject = _pool.CreateObject((FallObjectType)type);
+        newObject.gameObject.transform.position = spawnPoints[spawnPoint].position;
+        newObject.OnDeathEvent += _pool.ReturnToPool;
+    }
+}
