a5aa9419f8068b10175acea599ed7f2818fec78a
Keinih Danil <keinihd@gmail.com>
2023-08-10 10:44:01 +0700

    add Command spawn player, update PlayerMVC & AppSt

--- Assets/Academy-Platformer/ApplicationStartup.cs
+++ Assets/Academy-Platformer/ApplicationStartup.cs
@@ -1,4 +1,6 @@
 ﻿using Bootstrap;
+using Command;
+using FactoryPlayer;
 using UnityEngine;
 
 namespace ApplicationStartup
@@ -6,9 +8,12 @@ namespace ApplicationStartup
     public class ApplicationStartup : MonoBehaviour
     {
         private IBootstrap _bootstrap = new Bootstrap.Bootstrap();
-
+        private PlayerController _playerController;
+        private PlayerStorage _playerStorage = new PlayerStorage();
+        
         private void Start()
         {
+            _playerController = new PlayerController();
             StartBootstrap();
             Create<TickableManager>(ResourcesConst.ResourcesConst.TickableManager);
         }
@@ -22,6 +27,7 @@ namespace ApplicationStartup
 
         private void StartBootstrap()
         {
+            _bootstrap.Add(new CommandPlayerSpawn(_playerController, _playerStorage));
             _bootstrap.OnExecuteAllComandsNotify += NotifyOfCompletion;
             _bootstrap.Execute();
         }
---
+++ Assets/Academy-Platformer/Command/CommandPlayerSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FactoryPlayer;
+
+namespace Command
+{
+    public class CommandPlayerSpawn : Command
+    {
+        public override event Action OnCommandExecuteNotify;
+
+        private PlayerController _playerController;
+        private PlayerStorage _playerStorage;
+
+        public CommandPlayerSpawn(PlayerController playerController, PlayerStorage playerStorage)
+        {
+            _playerController = playerController;
+            _playerStorage = playerStorage;
+        }
+        
+        public override void Execute()
+        {
+            _playerStorage.Add(_playerController.Spawn());
+            
+            OnCommandExecuteNotify?.Invoke();
+        }
+
+        public override void Undo()
+        { }
+    }
+}
