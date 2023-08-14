42ba074f53eee807e532ef570361cfcff9aaf85e
unknown <l>
2023-08-14 10:27:34 +0700

    Add methods to start and finish game.

--- Assets/Academy-Platformer/GameController/GameController.cs
+++ Assets/Academy-Platformer/GameController/GameController.cs
@@ -8,14 +8,26 @@ public class GameController
     private FallObjectSpawner _spawner;
 
     private PlayerController _playerController;
+
+    private PlayerView _playerView;
     
     public GameController(FallObjectSpawner spawner,
         PlayerController playerController)
     {
         _spawner = spawner;
         _playerController = playerController;
-        _playerController.Spawn();
+    }
+
+    public void StartGame()
+    {
+        _playerView = _playerController.Spawn();
         TickableManager.UpdateEventHandler += Update;
+    }
+
+    public void StopGame()
+    {
+        GameObject.Destroy(_playerView.gameObject);
+        TickableManager.UpdateEventHandler -= Update;
     }
     
     void Update()
