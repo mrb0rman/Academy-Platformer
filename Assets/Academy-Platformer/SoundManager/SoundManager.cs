144310bd72fe6ec01e489c6ad436a61569c3b4b2
l buyan <blyundup@gmail.com>
2023-08-15 14:33:19 +0700

    add SoundManager script

--- Assets/Academy-Platformer/SoundManager/SoundManager.cs
+++ Assets/Academy-Platformer/SoundManager/SoundManager.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    
+    [SerializeField] private AudioSource[] _musicSources;
+    [SerializeField] private AudioSource[] _effectSources;
+
+    public void PlaySound(AudioClip clip)
+    {
+        // select an audio source and call the Play() method
+    }
+
+    public void ChangeMasterVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
 }
