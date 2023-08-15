459a5f92b5691e365721c39cf8c0df5bafeaaa24
unknown <l>
2023-08-15 12:52:43 +0700

    Fix codestyle

--- Assets/Academy-Platformer/FallObject/FallObjectMVC/FallObjectAnimator.cs
+++ Assets/Academy-Platformer/FallObject/FallObjectMVC/FallObjectAnimator.cs
@@ -3,18 +3,18 @@ using DG.Tweening;
 
 public class FallObjectAnimator
 {
-    private const float _spawnAnimDuration = 1f;
-    private const float _deathAnimDuration = 1f;
+    private const float SpawnAnimDuration = 1f;
+    private const float DeathAnimDuration = 1f;
     
     private Sequence _sequenceSpawn;
     private Sequence _sequenceDeath;
 
-    public void PlaySpawn(FallObjectView view)
+    public void Spawn(FallObjectView view)
     {
         //insert spawn animation here
     }
 
-    public void PlayDeath(FallObjectView view)
+    public void Death(FallObjectView view)
     {
         //insert death animation here
     }
