a4cd2705e6c2d4f261303ff92ea69d5f928885df
l buyan <blyundup@gmail.com>
2023-08-16 10:10:07 +0700

    remove extra functionality

--- Assets/Academy-Platformer/SoundManager/SoundManager.cs
+++ Assets/Academy-Platformer/SoundManager/SoundManager.cs
@@ -4,14 +4,4 @@ public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioSource[] _musicSources;
     [SerializeField] private AudioSource[] _effectSources;
-
-    public void PlaySound(AudioClip clip)
-    {
-        // select an audio source and call the Play() method
-    }
-
-    public void ChangeMasterVolume(float value)
-    {
-        AudioListener.volume = value;
-    }
 }
