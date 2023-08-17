d583bf0d8e2c66874090c380e529adf858373582
Keinih Danil <keinihd@gmail.com>
2023-08-17 12:51:25 +0700

    create Sound Pool and Config

---
+++ Assets/Academy-Platformer/SoundPool/SoundConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoundPool
+{
+    [CreateAssetMenu(fileName = "SoundConfig", menuName = "Configs/SoundConfig", order = 2)]
+    public class SoundConfig : ScriptableObject
+    {
+        
+    }
+
+    [Serializable]
+    public struct SoundModel
+    {
+        public AudioSource Sound;
+        
+        public string nameSound;
+    }
+}
---
+++ Assets/Academy-Platformer/SoundPool/SoundPool.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoundPool
+{
+    public class SoundPool
+    {
+        private Dictionary<string, AudioSource> _dictSound;
+
+        public SoundPool() 
+        {
+            _dictSound = new Dictionary<string, AudioSource>();
+        }
+
+        public AudioSource TakeFromPool(string nameSound)
+        {
+            if (!_dictSound.ContainsKey(nameSound))
+            {
+                Debug.Log($"Sound with the name {nameSound} not found");
+                return null;
+            }
+            
+            return _dictSound[nameSound];
+        }
+    }
+}
